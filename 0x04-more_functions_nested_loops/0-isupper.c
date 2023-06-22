a2c0c0034e550e7e841d5c740a02a4bccb2d767a
Ruttoz <sharonrutto95@gmail.com>
2023-06-22 06:44:04 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - check if a letter is upper
+ * _isupper - Check if a letter is upper
  * @a: The number to be checked
  * Return: 1 for upper letter or 0 for any elese
  */
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * 1-isdigit.c - Check if a character is a digit
+ * @x: The number to be checked
+ * Return: 1 for a character that will be a digit or 0 for any else
+ */
+
+int 1-isdigit.c(int x)
+
+{
+	if (x >= 48 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
