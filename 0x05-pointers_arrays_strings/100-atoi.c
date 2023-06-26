8f470f1f16f9d6c5106a58a1377ee5171bb64b73
Ruttoz <sharonrutto95@gmail.com>
2023-06-26 15:40:25 -0700

    fixed the replication bug

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,12 @@ int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
-	f = 0; digit = 0; len = 0; i = 0; d = 0; n = 0;
+	f = 0;
+       	digit = 0;
+       	len = 0;
+       	i = 0;
+       	d = 0;
+       	n = 0;
 	
 	while (s[len] != '\0')
 		len++;
