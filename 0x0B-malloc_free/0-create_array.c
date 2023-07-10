8e9082afdc597c963ae1b9ed90274ecc17a77273
Ruttoz <sharonrutto95@gmail.com>
2023-07-10 07:48:03 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,12 +3,11 @@
 
 /**
  * create_array - creates an array of chars
- * @s: The size of the array
+ * @size: The size of the array
  * @c: The char to fill in the array
  *
  * Return: The array filled
  */
-
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
