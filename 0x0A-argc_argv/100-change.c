fedba1a3e397afcba2c35e7a641ff183f043649a
Ruttoz <sharonrutto95@gmail.com>
2023-07-10 01:32:39 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -21,15 +21,13 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	num = _atoi(argv[1]);
+	num = atoi(argv[1]);
 	result = 0;
-
 	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-
 	for (j = 0; j < 5 &&  num >= 0; j++)
 	{
 		while (num >= coins[j])
@@ -38,7 +36,6 @@ int main(int argc, char *argv[])
 			num -= coins[j];
 		}
 	}
-
 	printf("%d\n", result);
 	return (0);
 }
